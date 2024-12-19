9be418f9e9bb6fdf61ecd4a1b42f7e0d0e1bf4e6
techguyseli <kassouselaiman@gmail.com>
2024-12-19 20:35:09 +0100

    added lombok to jdtls

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,4 +1,5 @@
 local lsp_zero = require('lsp-zero')
+local home = os.getenv("HOME")
 
 lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
@@ -7,6 +8,7 @@ lsp_zero.on_attach(function(client, bufnr)
 
     vim.keymap.set('n', ';;', vim.lsp.buf.code_action, {buffer = bufnr})
 end)
+
 
 -- to learn how to use mason.nvim
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
@@ -19,7 +21,26 @@ require('mason-lspconfig').setup({
     },
     handlers = {
         function(server_name)
-            require('lspconfig')[server_name].setup({})
+            if server_name == "jdtls" then
+                require('lspconfig').jdtls.setup({
+                    cmd = {
+                        'java',
+                        '-javaagent:' .. home .. '/.local/share/nvim/mason/packages/jdtls/lombok.jar',
+                        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+                        '-Dosgi.bundles.defaultStartLevel=4',
+                        '-Declipse.product=org.eclipse.jdt.ls.core.product',
+                        '-Dlog.level=ALL',
+                        '-Xms1g',
+                        '-Xmx2G',
+                        '-jar', home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar',
+                        '-configuration', home .. '/.local/share/nvim/mason/packages/jdtls/config_linux',
+                        '-data', home .. '/.workspace/jdtls'
+                    },
+                    root_dir = require('lspconfig.util').root_pattern(".git", "mvnw", "gradlew", "pom.xml", "build.gradle"),
+                })
+            else
+                require('lspconfig')[server_name].setup({})
+            end
         end,
     },
 })
